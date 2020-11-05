de7a33f57594a12006f7970e6747d0589615f013
RajPathare <rajgpathare@gmail.com>
2020-11-05 15:17:42 +0530

    reverted change

--- src/index.js
+++ src/index.js
@@ -24,64 +24,64 @@ app.get('*',(req,res)=>{
 })
 
 
-// const client = new Discord.Client();
-// client.login(secrets.BOT_TOKEN);
+const client = new Discord.Client();
+client.login(secrets.BOT_TOKEN);
 
-// client.on("message", (message)=>{
-//     if(message.author.bot) return;
-//     if(!message.content.startsWith(prefix)) return;
+client.on("message", (message)=>{
+    if(message.author.bot) return;
+    if(!message.content.startsWith(prefix)) return;
 
-//     const commandBody = message.content.slice(prefix.length);
-//     console.log(commandBody);
-//     const args = commandBody.split(' ');
-//     console.log('ARGS',args);
-//     console.log(args[0], args[1], args[2]);
-//     const command = args.shift().toLowerCase();
+    const commandBody = message.content.slice(prefix.length);
+    console.log(commandBody);
+    const args = commandBody.split(' ');
+    console.log('ARGS',args);
+    console.log(args[0], args[1], args[2]);
+    const command = args.shift().toLowerCase();
 
-//     console.log(command);
+    console.log(command);
 
-//     if(command === "ping"){
-//         const timeTaken = Date.now() - message.createdTimestamp;
-//         message.reply(`pong, Latency - ${timeTaken}ms`);
-//     }
+    if(command === "ping"){
+        const timeTaken = Date.now() - message.createdTimestamp;
+        message.reply(`pong, Latency - ${timeTaken}ms`);
+    }
     
-// })
+})
 
-// client.on('ready',()=>{
-//     cron.schedule('30 11 * * *',()=>{
-//         console.log('call trigger!');
-//         sendMessageForStandup();
-//     })
-//     cron.schedule('00 10 * * *',()=>{
-//         console.log('markin trigger!');
-//         sendMessageForMarkIn();
-//     })
-//     cron.schedule('30 19 * * *',()=>{
-//         console.log('markout trigger!');
-//         sendMessageForMarkOut();
-//     })
-// })
+client.on('ready',()=>{
+    cron.schedule('30 11 * * *',()=>{
+        console.log('call trigger!');
+        sendMessageForStandup();
+    })
+    cron.schedule('00 10 * * *',()=>{
+        console.log('markin trigger!');
+        sendMessageForMarkIn();
+    })
+    cron.schedule('30 19 * * *',()=>{
+        console.log('markout trigger!');
+        sendMessageForMarkOut();
+    })
+})
 
-// const sendMessageForStandup = async () => {
-//     var guild = client.guilds.cache.get('689367318345809920');
-//     if(guild && guild.channels.cache.get('689367318345809923')){
-//         guild.channels.cache.get('689367318345809923').send("@everyone Call pe chalo!!!");
-//     }
-// }
+const sendMessageForStandup = async () => {
+    var guild = client.guilds.cache.get('689367318345809920');
+    if(guild && guild.channels.cache.get('689367318345809923')){
+        guild.channels.cache.get('689367318345809923').send("@everyone Call pe chalo!!!");
+    }
+}
 
-// const sendMessageForMarkIn = async () => {
-//     var guild = client.guilds.cache.get('689367318345809920');
-//     if(guild && guild.channels.cache.get('689367318345809923')){
-//         guild.channels.cache.get('689367318345809923').send("@everyone MARK IN!!!!");
-//     }
-// }
+const sendMessageForMarkIn = async () => {
+    var guild = client.guilds.cache.get('689367318345809920');
+    if(guild && guild.channels.cache.get('689367318345809923')){
+        guild.channels.cache.get('689367318345809923').send("@everyone MARK IN!!!!");
+    }
+}
 
-// const sendMessageForMarkOut = async () => {
-//     var guild = client.guilds.cache.get('689367318345809920');
-//     if(guild && guild.channels.cache.get('689367318345809923')){
-//         guild.channels.cache.get('689367318345809923').send("@everyone MARK OUT!!!");
-//     }
-// }
+const sendMessageForMarkOut = async () => {
+    var guild = client.guilds.cache.get('689367318345809920');
+    if(guild && guild.channels.cache.get('689367318345809923')){
+        guild.channels.cache.get('689367318345809923').send("@everyone MARK OUT!!!");
+    }
+}
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
