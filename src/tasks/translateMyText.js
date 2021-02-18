c56a8e21f20ae46e8916afffafec43bb7ec6c410
RajPathare <rajgpathare@gmail.com>
2021-02-18 13:27:48 +0530

    added full language support

--- src/tasks/translateMyText.js
+++ src/tasks/translateMyText.js
@@ -1,6 +1,7 @@
 const translate = require('@vitalets/google-translate-api');
+const getISOcode = require('./getISOcode');
 
-const translateMyText = async (args,message,ISO) => {
+const translateMyText = async (args,message,language) => {
     console.log('invoked translate text function!');
 
     if(args[1] === undefined)
@@ -18,9 +19,16 @@ const translateMyText = async (args,message,ISO) => {
             myQuestion = myQuestion.join(' ');
             console.log(myQuestion);
 
-            const text = await translate(myQuestion, {to: ISO});
-            console.log('FINAL text - ',text);
-            message.channel.send(text.text);
+            let ISOcode = getISOcode(language);
+            if(ISOcode === 0) {
+                console.log("Hmm.. I couldn't understand the language :(");
+                message.channel.send("Hmm.. I couldn't understand the language :(");
+            }
+            else {
+                const text = await translate(myQuestion, {to: ISOcode});
+                console.log('FINAL text - ',text.text);
+                message.channel.send(text.text);
+            }
         }
         catch (e)
         {
